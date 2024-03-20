83186b0f4a552d43409a056b06eff5fc1a1d1550
imMarina <m.shebukhova@yandex.com>
2024-03-20 21:59:36 +0100

    Main - changed name of classes + scss

--- src/js/task_14.2/main.js
+++ src/js/task_14.2/main.js
@@ -14,18 +14,18 @@ for (let i = 0; i < 2; i++) {
 }
 
 // Добавляем class для DIV 
-mainContainer.children[0].classList.add('main-container__top');
-mainContainer.children[1].classList.add('main-container__bottom');
+mainContainer.children[0].classList.add('container__top');
+mainContainer.children[1].classList.add('container__bottom');
 
 
 // <div class="main-container__top">
 // Создаем NAV боковое меню
 const navMenu = document.createElement('nav');
-navMenu.classList.add('main__nav-menu');
+navMenu.classList.add('nav-menu');
 // console.log(navMenu); // <nav class="main__nav-menu"></nav>
 
 const navMenuTPL = `
-    <ul class="main__nav-menu__list-items">
+    <ul class="list-items">
         <li>
             <a class="selected" href="#!">Home</a>
         </li>
@@ -54,8 +54,8 @@ const sectionIMG = document.createElement('section');
 sectionIMG.classList.add('sections-images'); // <section class="sections-images">
 
 const sectionImgTPL = `
-    <div class="container-section-img1">
-        <div class="container-section-img2"></div>
+    <div class="img1">
+        <div class="img2"></div>
     </div>    
 `;
 sectionIMG.insertAdjacentHTML('beforeend', sectionImgTPL);
@@ -66,7 +66,7 @@ mainContainer.children[0].append(sectionIMG);
 
 // Создаем SECTION TEXT
 const sectionText = document.createElement('section');
-sectionText.classList.add('main__sections-text'); // <section class="main__sections-text">
+sectionText.classList.add('sections-text'); // <section class="sections-text">
 
 // Создаем 3 элемента DIV контейнера
 for (let i = 0; i < 3; i++) {
@@ -75,13 +75,13 @@ for (let i = 0; i < 3; i++) {
 }
 
 // добавляем class для DIV 
-sectionText.children[0].classList.add('container__section-text'); // <div class="container__section-text">
-sectionText.children[1].classList.add('container__section-buttons'); // <div class="container__section-buttons">
-sectionText.children[2].classList.add('container__section-statistics'); // <div class="container__section-statistics">
+sectionText.children[0].classList.add('container-text'); // <div class="container__section-text">
+sectionText.children[1].classList.add('container-buttons'); // <div class="container__section-buttons">
+sectionText.children[2].classList.add('container-statistics'); // <div class="container__section-statistics">
 // console.log(sectionText);
 
 
-// Наполняем контентом <div class="container__section-text">
+// Наполняем контентом <div class="container-text">
 const sectionTextTPL = `
     <h1>We’re a Creative Design Agency</h1>
     <p>We are delivering top level digital services best experlenced team, just get stared</p>  
@@ -89,34 +89,34 @@ const sectionTextTPL = `
 sectionText.children[0].insertAdjacentHTML('beforeend', sectionTextTPL);
 
 
-// Наполняем контентом <div class="container__section-buttons">
+// Наполняем контентом <div class="container-buttons">
 const sectionBtnTPL = `
-    <a href="!#" class="container__section-btn-1">
+    <a href="!#" class="btn-1">
         <span>Get started</span>
-        <span class="container__section-btn-1__img"></span>
+        <span class="btn-1__img"></span>
     </a>
 
-    <a href="!#" class="container__section-btn-2">
-        <span class="container__section-btn-2__img"></span>
+    <a href="!#" class="btn-2">
+        <span class="btn-2__img"></span>
         <span>How it works</span>
     </a> 
 `;
 sectionText.children[1].insertAdjacentHTML('beforeend', sectionBtnTPL);
 
 
-// Наполняем контентом <div class="container__section-statistics">
+// Наполняем контентом <div class="container-statistics">
 const sectionStatisticsTPL = `
-    <div class="container__section-statistics__items">
-        <span class="item-1__number">20+</span>
-        <span class="item-1__text">Years of  Excellent</span>
+    <div class="items">
+        <span class="number">20+</span>
+        <span class="text">Years of  Excellent</span>
     </div>
-    <div class="container__section-statistics__items">
-        <span class="item-2__number">90%</span>
-        <span class="item-2__text">Happy  Client</span>
+    <div class="items">
+        <span class="number">90%</span>
+        <span class="text">Happy  Client</span>
     </div>
-    <div class="container__section-statistics__items">
-        <span class="item-3__number">80%</span>
-        <span class="item-3__text">Project  Succes</span>
+    <div class="items">
+        <span class="number">80%</span>
+        <span class="text">Project  Succes</span>
     </div>
 `;
 sectionText.children[2].insertAdjacentHTML('beforeend', sectionStatisticsTPL);
@@ -126,24 +126,24 @@ sectionText.children[2].insertAdjacentHTML('beforeend', sectionStatisticsTPL);
 mainContainer.children[0].append(sectionText);
 
 
-// <div class="main-container__bottom">
+// <div class="container__bottom">
 // Создаем SECTION
 const sectionClientsList = document.createElement('section');
-sectionClientsList.classList.add('container-section__clients-list');
-// console.log(sectionClientsList); // <section class="container-section__clients-list"></section>
+sectionClientsList.classList.add('clients-list');
+// console.log(sectionClientsList); // <section class="clients-list"></section>
 
 const sectionClientsListTPL = `
-    <a class="clients-list__item-1" href="!#"></a>
+    <a class="item-1" href="!#"></a>
 
-    <a class="clients-list__item-2" href="!#"></a>
-    <a class="clients-list__item-3" href="!#"></a>
-    <a class="clients-list__item-4" href="!#"></a>
-    <a class="clients-list__item-5" href="!#"></a>
+    <a class="item-2" href="!#"></a>
+    <a class="item-3" href="!#"></a>
+    <a class="item-4" href="!#"></a>
+    <a class="item-5" href="!#"></a>
 `;
 sectionClientsList.insertAdjacentHTML('beforeend', sectionClientsListTPL);
 
 
-// Добавляем SECTION в DOM в <div class="main-container__bottom">
+// Добавляем SECTION в DOM в <div class=container__bottom">
 mainContainer.children[1].append(sectionClientsList);
 
 
